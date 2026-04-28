abbd4e70206a5127be0e911dd40ee7cec604a081
Sandali Disara <sandalidissanayake123@gmail.com>
2026-04-28 15:30:14 +0530

    add services section componenet

--- nova-website/app/page.tsx
+++ nova-website/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
 import AboutSection from "@/components/AboutSection/AboutSection";
+import ServicesSection from "@/components/ServicesSection/ServicesSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ServicesSection />
     </main>
   );
 }
